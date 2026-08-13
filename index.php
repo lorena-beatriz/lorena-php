04506072f551b5a9da66d44a861abd5fe97eb0ca
lorena-beatriz <274602@unifio.edu.br>
2026-08-13 01:30:45 +0000

    adicionado o index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+$nome = "Lorena"; // String (texto)
+$idade = "18";    // Integer (Inteiro)
+$peso = "62";     // Float (Decimal)
+$vivo = true;     // Boolean (Verdadeiro/Falso)
+
+
+echo "Meu nome é $nome, tenho $idade anos de idade"
+
+?>
